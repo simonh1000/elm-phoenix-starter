2dbceca13fa561e1c11712ceae3174ccb3427f3b
Simon Hampton <hotbelgo@gmail.com>
2016-08-14 09:24:12 +0200

    use phoenix template

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,7 +2,7 @@ defmodule Meepg.PageController do
   use Meepg.Web, :controller
 
   def index(conn, _params) do
-    # render conn, "index.html"
-    html(conn, File.read!("priv/static/index.html"))
+    render conn, "index.html"
+    # html(conn, File.read!("priv/static/index.html"))
   end
 end
